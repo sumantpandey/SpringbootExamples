d46b0f69909e2b906f09f39d5e8d249acb817e7a
sumant <you@example.com>
2020-11-02 02:36:01 +0530

    initial commit

---
+++ spring-rest-error-handling/src/main/java/com/skp/error/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.skp.error;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
